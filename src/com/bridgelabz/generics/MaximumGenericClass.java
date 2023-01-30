9d0aa678bc0241b2d83d3a4e1cb7d5aef592a525
palle146github <112770521+palle146github@users.noreply.github.com>
2023-01-30 22:25:16 +0530

    Refactor2: Finding maxim using Generic Class

--- src/com/bridgelabz/generics/MaximumGenericClass.java
+++ src/com/bridgelabz/generics/MaximumGenericClass.java
@@ -1,15 +1,25 @@
 package com.bridgelabz.generics;
 
 import java.util.Scanner;
-public class MaximumGeneric {
 
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-        T max = x;
-        if (y.compareTo(x) > 0) {
-            max = y;
+public abstract class MaximumGenericClass<E> implements Comparable<E> {
+
+    E element1, element2, element3;
+
+    public MaximumGenericClass(E element1, E element2, E element3) {
+        this.element1 = element1;
+        this.element2 = element2;
+        this.element3 = element3;
+    }
+
+    public static <E extends Comparable<E>> E testMaximum(E element1, E element2, E element3) {
+        System.out.println("The numbers are : " + element1 + ",  " + element2 + ",   " + element3);
+        E max = element1;
+        if (max.compareTo(element2) > 0) {
+            max = element2;
         }
-        if (z.compareTo(max) > 0) {
-            max = z;
+        if (max.compareTo(element3) > 0) {
+            max = element3;
         }
         return max;
     }
@@ -26,7 +36,7 @@ public class MaximumGeneric {
         System.out.println("Enter third integer Number");
         int integer3 = s.nextInt();
 
-        int integerMax = maximum(integer1, integer2, integer3);
+        int integerMax = testMaximum(integer1, integer2, integer3);
         System.out.println("Greatest number of integer type is : " + integerMax);
         System.out.println();
 
@@ -39,9 +49,10 @@ public class MaximumGeneric {
         System.out.println("Enter third float Number");
         float float3 = s.nextFloat();
 
-        float floatMax = maximum(float1, float2, float3);
-        System.out.println("Greatest number of float type is : " + floatMax);
+        float floatMax = testMaximum(float1, float2, float3);
+        System.out.println("Greatest of float number is : " + floatMax);
         System.out.println();
+
 
         System.out.println("Enter first String");
         String string1 = s.next();
@@ -52,8 +63,9 @@ public class MaximumGeneric {
         System.out.println("Enter third String");
         String string3 = s.next();
 
-        String stringMax = maximum(string1, string2, string3);
-        System.out.println("Greatest string type is : " + stringMax);
+        String stringMax = testMaximum(string1, string2, string3);
+        System.out.println("Greatest String among three strings is : " + stringMax);
         System.out.println();
     }
 }
+
