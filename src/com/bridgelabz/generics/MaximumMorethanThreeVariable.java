75169f1c3a6a502649d7ba41e29e0bf223be4eec
palle146github <112770521+palle146github@users.noreply.github.com>
2023-01-30 23:32:06 +0530

    Added: Maximum among the list of elements

--- src/com/bridgelabz/generics/MaximumMorethanThreeVariable.java
+++ src/com/bridgelabz/generics/MaximumMorethanThreeVariable.java
@@ -2,7 +2,8 @@ package com.bridgelabz.generics;
 
 import java.util.List;
 import java.util.Arrays;
-public abstract class MaximumMorethanThreeVariable<T> implements Comparable<T> {
+
+public abstract class MaximumMorethanThreeVariable<E extends Comparable<E>> {
 
 
     public static <E extends Comparable<E>> E testMaximum(List<E> list) {
@@ -13,13 +14,19 @@ public abstract class MaximumMorethanThreeVariable<T> implements Comparable<T> {
         }
         return max;
     }
-    public static void main(String[] args) {
+
+    public static <E extends Comparable<E>> E printMax() {
 
         int maxInt = testMaximum(Arrays.asList(4, 6, 7, 19, 43, 18, 35, 18, 94));
         System.out.println("Maximum integer is " + maxInt);
-        float maxFloat = testMaximum(Arrays.asList( 4.6F, 7.4F, 8.9F, 3.5F, 6.9F, 3.9F));
+        float maxFloat = testMaximum(Arrays.asList(4.6F, 7.4F, 8.9F, 3.5F, 6.9F, 3.9F));
         System.out.println("Maximum Float is " + maxFloat);
-        String maxString = testMaximum(Arrays.asList( "Hello", "Great", "Bad", "Good", "Secret", "David", "John" ));
+        String maxString = testMaximum(Arrays.asList("Hello", "Great", "Bad", "Good", "Secret", "David", "John"));
         System.out.println("Maximum String is " + maxString);
+        return null;
+    }
+
+    public static void main(String[] args) {
+        printMax();
     }
 }
