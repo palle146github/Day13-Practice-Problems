686569fc1a3e9b819ad479bf65159157f874893e
palle146github <112770521+palle146github@users.noreply.github.com>
2023-01-30 23:20:14 +0530

    Added: Maximum among the list of elements

---
+++ src/com/bridgelabz/generics/MaximumMorethanThreeVariable.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.generics;
+
+import java.util.List;
+import java.util.Arrays;
+public abstract class MaximumMorethanThreeVariable<T> implements Comparable<T> {
+
+
+    public static <E extends Comparable<E>> E testMaximum(List<E> list) {
+        E max = list.get(0);
+        for (E element : list) {
+            if (element.compareTo(max) > 0)
+                max = element;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+
+        int maxInt = testMaximum(Arrays.asList(4, 6, 7, 19, 43, 18, 35, 18, 94));
+        System.out.println("Maximum integer is " + maxInt);
+        float maxFloat = testMaximum(Arrays.asList( 4.6F, 7.4F, 8.9F, 3.5F, 6.9F, 3.9F));
+        System.out.println("Maximum Float is " + maxFloat);
+        String maxString = testMaximum(Arrays.asList( "Hello", "Great", "Bad", "Good", "Secret", "David", "John" ));
+        System.out.println("Maximum String is " + maxString);
+    }
+}
